d5b3fff233ecc184a9cdd0c8025225659e86b163
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-11 15:52:56 +0100

    aggiunto logica in js

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const authorizedMails = ["matteo@gmaiil.com", "fabio@gmail.com", "pietro@gmail.com","anna@gmail.com"];
 
+let mailInput = document.getElementById("mailInput");
+let button = document.getElementById("mailButton");
+button.addEventListener("click", ()=>{
+   let mailInputValue = mailInput.value;
+   let authorized =false;
+
+   for(let i =0; i<authorizedMails.length; i++){
+    if(authorizedMails[i]==mailInputValue){
+authorized = true;
+console.log("you are authorized");
+break;
+    }
+   }
+if(!authorized){
+    console.log("this email is not authorized");
+}
+
+} )
