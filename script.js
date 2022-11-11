d6cf6e8c44e42984f1e1719d44557f37ae275471
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-11 16:06:16 +0100

    modificato form con inputDiv

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ const authorizedMails = ["matteo@gmaiil.com", "fabio@gmail.com", "pietro@gmail.c
 
 let mailInput = document.getElementById("mailInput");
 let button = document.getElementById("mailButton");
+
+
 button.addEventListener("click", ()=>{
    let mailInputValue = mailInput.value;
    let authorized =false;
+   console.log(mailInputValue);
 
    for(let i =0; i<authorizedMails.length; i++){
-    if(authorizedMails[i]==mailInputValue){
+    if(authorizedMails[i] == mailInputValue){
 authorized = true;
 console.log("you are authorized");
 break;
     }
    }
-if(!authorized){
+
+   if(!authorized){
     console.log("this email is not authorized");
 }
 
